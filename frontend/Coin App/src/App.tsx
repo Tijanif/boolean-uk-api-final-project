6f663e75dc561faba2a9b6a33feb38f3041c4723
Tijani Fulani <Fulani07@gmail.com>
2021-08-18 14:43:46 +0100

    initialised vite app for frontend

---
+++ frontend/Coin App/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+        <h1>Hello Coin App</h1>
+    </div>
+  )
+}
+
+export default App
